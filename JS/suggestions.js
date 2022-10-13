710641607d03c0f8784dfaa0fc5bc9368e7c17f3
nguyentrongnhan2022 <114295992+nguyentrongnhan2022@users.noreply.github.com>
2022-10-13 19:19:38 +0700

    Fix code

---
+++ JS/suggestions.js
@@ -0,0 +1,14 @@
+let suggestions = [
+    "Máy ảnh Sony A6000",
+    "Máy ảnh Sony A6300",
+    "Máy ảnh Sony A6400",
+    "Máy ảnh Sony A6500",
+    "Máy ảnh Sony A7 Mark 2",
+    "Máy ảnh Sony A7 Mark 3",
+    "Máy ảnh Sony ZV-1",
+    "Ống kính FE 85mm F1.8",
+    "Ống kính FE 50mm F1.8",
+    "Ống kính FE 55mm Cz F1.8",
+    "Ống kính SEL 50mm F1.8",
+    "Ống kính FE 24-70GM F2.8",
+];
