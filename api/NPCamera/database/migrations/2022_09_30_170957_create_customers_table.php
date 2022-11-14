865348a5303de50db56645a073cd03e36753f01d
Phong Kim Do <94376574+Phongdo1104@users.noreply.github.com>
2022-11-14 13:49:38 +0700

    Removed subscribed field from Customer table

--- api/NPCamera/database/migrations/2022_09_30_170957_create_customers_table.php
+++ api/NPCamera/database/migrations/2022_09_30_170957_create_customers_table.php
@@ -24,7 +24,6 @@ return new class extends Migration
             // this "default_avatar" column is temporary
             $table->string("default_avatar")->default("https://i.kym-cdn.com/photos/images/facebook/001/921/416/50b");
             // $table->string("phone_number");
-            $table->boolean("subscribed")->comment("SubscribeValueEnum");
             $table->boolean("disabled")->nullable()->comment("Default value is NULL, 0 for disable");
             // $table->rememberToken();
             $table->timestamps();
