8f73671b2e6360bca07ba0db0d0b664a1eb690da
Darino <94376574+Phongdo1104@users.noreply.github.com>
2022-11-15 00:42:58 +0700

    Created trending query and some more

--- api/NPCamera/app/Http/Controllers/Api/V1/ProductQueryController.php
+++ api/NPCamera/app/Http/Controllers/Api/V1/ProductQueryController.php
@@ -4,7 +4,9 @@ namespace App\Http\Controllers\Api\V1;
 
 use App\Http\Controllers\Controller;
 use App\Http\Resources\V1\ProductListCollection;
+use App\Models\Order;
 use App\Models\Product;
+use DateTime;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
@@ -12,22 +14,6 @@ class ProductQueryController extends Controller
 {
     public function arrival()
     {
-
-        $file = explode("\\", dirname(__FILE__));
-        $path_get = $file[0];
-        for ($i = 1; $i < sizeof($file) - 1; $i++) {
-            $path_get = $path_get . "\\" . $file[$i];
-        }
-
-        $path = $path_get . "\\" . "camera.json";
-
-        // Read the JSON file 
-        $json = file_get_contents($path);
-
-        // Decode the JSON file
-        $json_data = json_decode($json, true);
-        dd($json_data[0]);
-
         $products = Product::orderBy("created_at", "DESC")->take(10)->get();
 
         return new ProductListCollection($products);
@@ -43,6 +29,7 @@ class ProductQueryController extends Controller
 
     public function best()
     {
+        // Count duplicate products
         $products_filter = DB::table("order_product")
             ->select("product_id", DB::raw('count(product_id) as count'))
             ->groupBy('product_id')
@@ -67,5 +54,110 @@ class ProductQueryController extends Controller
         }
 
         return $products_best_seller;
+    }
+
+    public function trending(Request $request)
+    {
+        $orders = Order::orderBy("created_at", "ASC")->take(100)->get();
+
+        // Array used to store 100 orders ordered by "created_at" in ascending
+        $arr_orders = [];
+        $arr_orders_2 = [];
+        $current_date = date("Y-m-d H:i:s");
+        $index = 0;
+
+        // Get all recent orders meet the require days (base on {day} calculated on second)
+        for ($i = 0; $i < sizeof($orders); $i++) {
+            if ((strtotime($current_date) - strtotime($orders[$i]['created_at'])) >= ((int) $request->day) * 60 * 60 * 24) {
+                // $arr_orders[] = $orders[$i]['id'];
+                $arr_orders[] = $orders[$i]['id'];
+                $arr_orders_2[] = strtotime($current_date) - strtotime($orders[$i]['created_at']);
+                $index++;
+            }
+        }
+
+        $arr_products = [];
+        // Get all products store in order
+        for ($i = 0; $i < sizeof($arr_orders); $i++) {
+            // dd($arr_orders[$i]);
+            $products_in_order = DB::table("order_product")
+                ->select("product_id")
+                ->where("order_id", "=", $arr_orders[$i])
+                ->get();
+
+            if ($products_in_order->count() === 0) {
+                // return false and quit
+                continue;
+            }
+
+            if (sizeof($arr_products) === 0) { // Run first time
+                for ($j = 0; $j < sizeof($products_in_order); $j++) {
+                    $arr_products[] = $products_in_order[$j]->product_id;
+                }
+                continue;
+            }
+
+            for ($k = 0; $k < sizeof($products_in_order); $k++) {
+                // dd($products_in_order[$k]);
+                if (!in_array($products_in_order[$k]->product_id, $arr_products)) {
+                    $arr_products[] = $products_in_order[$k]->product_id;
+                }
+            }
+        }
+
+        $arr_products_filter = [];
+        $index = 0;
+
+        // Check if array is below 10 then use it without product filter
+        if (sizeof($arr_products) <= 10) {
+            for ($i = 0; $i < sizeof($arr_products); $i++) {
+                $product = Product::where("id", "=", $arr_products[$i])->first();
+
+                if ($product->status === 0 || $product->deleted_at !== null) continue;
+
+                $arr_products_filter[$index]['id'] = $product->id;
+                $arr_products_filter[$index]['name'] = $product->name;
+                $arr_products_filter[$index]['description'] = $product->description;
+                $arr_products_filter[$index]['price'] = $product->price;
+                $arr_products_filter[$index]['img'] = $product->img;
+                $arr_products_filter[$index]['percentSale'] = $product->percent_sale;
+                $arr_products_filter[$index]['quantity'] = $product->quantity;
+                $index++;
+            }
+
+            return $arr_products_filter;
+        }
+
+        for ($i = 0; $i < sizeof($arr_products); $i++) {
+            $products_filter = DB::table("order_product")
+                ->select("product_id", DB::raw('count(product_id) as count'))
+                ->groupBy('product_id')
+                ->where("product_id", "=", $arr_products[$i])
+                ->orderBy('count', 'DESC')
+                ->get();
+
+            if ($products_filter[0]->count >= 2) {
+                $product = Product::where("id", "=", $products_filter[0]->product_id)->first();
+
+                if ($product->status === 0 || $product->deleted_at !== null) continue;
+
+                $arr_products_filter[$index]['id'] = $product->id;
+                $arr_products_filter[$index]['name'] = $product->name;
+                $arr_products_filter[$index]['description'] = $product->description;
+                $arr_products_filter[$index]['price'] = $product->price;
+                $arr_products_filter[$index]['img'] = $product->img;
+                $arr_products_filter[$index]['percentSale'] = $product->percent_sale;
+                $arr_products_filter[$index]['quantity'] = $product->quantity;
+                $index++;
+            }
+        }
+
+        // If after product filter there still no product then reuse on sale product function
+        if (sizeof($arr_products_filter) === 0) {
+            $this->sale();
+        }
+
+        // If everything went well then return result
+        return $arr_products_filter;
     }
 }
